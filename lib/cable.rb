37b4ce611b8f9095a98c1843f1aa2967e0247098
Spencer Markowski <spencer.markowski@gmail.com>
2010-11-24 15:23:52 -0500

    adds cable menu

--- lib/cable.rb
+++ lib/cable.rb
@@ -2,6 +2,8 @@ module Cable
   
   require 'cable/engine' if defined?(Rails) && Rails::VERSION::MAJOR == 3
   require 'cable/base'
+  require "cable/cable_menu/acts_as_cable_menu"
+  require "cable/cable_menu/acts_as_cable"
   
   puts "Initializing Cable #{Cable::Base.version}"
   
---
+++ lib/cable/cable_menu.rb
@@ -0,0 +1,5 @@
+# CableMenu
+# require 'active_support/core_ext'
+
+require "cable_menu/acts_as_cable_menu"
+require "cable_menu/acts_as_cable"
