0327c23cf4a1dc80f5612140e93d7ce346a2d903
Michael Bishop <mike@theablefew.com>
2010-12-23 00:15:39 -0600

    lots of style updates

--- public/javascripts/admin.js
+++ public/javascripts/admin.js
@@ -1,21 +1,19 @@
 $(document).ready(function(){
   $().ready(function() {
-    $('textarea').tinymce({
+		$('textarea.tinymce').tinymce({
       script_url : '/javascripts/tinymce/tiny_mce.js',
       theme : "advanced",
       height : "600px",
       plugins : "pagebreak,style,layer,table,save,advhr,advimage,advlink,emotions,iespell,inlinepopups,insertdatetime,preview,media,searchreplace,print,contextmenu,paste,directionality,fullscreen,noneditable,visualchars,nonbreaking,xhtmlxtras,template,advlist",
-      theme_advanced_buttons1 : "save,newdocument,|,bold,italic,underline,strikethrough,|,justifyleft,justifycenter,justifyright,justifyfull,formatselect",
-      theme_advanced_buttons2 : "cut,copy,paste,pastetext,pasteword,|,search,replace,|,bullist,numlist,|,outdent,indent,blockquote,|,undo,redo,|,link,unlink,anchor,image,cleanup,help,code,|,insertdate,inserttime,preview,|,forecolor,backcolor",
-      theme_advanced_buttons3 : "tablecontrols,|,hr,removeformat,visualaid,|,sub,sup,|,charmap,media,advhr,|,print,|,ltr,rtl,|,fullscreen",
+      theme_advanced_buttons1 : "bold,italic,underline,strikethrough,|,justifyleft,justifycenter,justifyright,justifyfull,formatselect,cut,copy,paste,pastetext,pasteword",
+      theme_advanced_buttons2 : "search,replace,|,bullist,numlist,|,outdent,indent,blockquote,|,undo,redo,|,link,unlink,anchor,image,cleanup,help,code,preview",
+      theme_advanced_buttons3 : "tablecontrols,|,hr,removeformat,visualaid,|,sub,sup,|,media|,fullscreen",
       theme_advanced_toolbar_location : "top",
       theme_advanced_toolbar_align : "left",
       theme_advanced_statusbar_location : "bottom",
       theme_advanced_resizing : true
     });
   });
-  
-  $('.button').button();
   $('.disabled-button').button({disabled: true});
   $('.yes_no :checkbox').iphoneStyle({
     checkedLabel: 'YES',
