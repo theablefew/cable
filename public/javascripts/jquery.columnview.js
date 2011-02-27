f133697ead6d761df78ae8d61f94d6a44368d55d
Michael Bishop <mike@theablefew.com>
2011-02-27 16:00:21 -0600

    wip

--- public/javascripts/jquery.columnview.js
+++ public/javascripts/jquery.columnview.js
@@ -12,78 +12,80 @@
  */
 
 (function($){
-
-  function deselect() {
-    var $ul = $(this).removeClass('selected').data('child');
-    if ($ul) {
-      $ul.find('li.selected').removeClass('selected').each(deselect);
-    }
+  
+  function position_menu(item){
+    console.log("Position Menu");
+    
+    var $this = item;
+    
+    var this_left = $this.parent().offset().left;
+    var panel_left = $this.parent().parent().offset().left;
+    var panel_scroll_left = $this.parent().parent().scrollLeft();
+    var left = (this_left-panel_left)+panel_scroll_left
+    var width = $this.parent().width();
+    
+    $this.data('child').css({
+      left: left + width,
+      top: 0
+    });
   }
   
-  function select_item(){
+  
+  function select_item(item){
+    console.log("Select Item");
+    console.log("Remove all selected");
+    
+    $(".selected").removeClass("selected");
+    $(".child_menu").hide();
+    
+    // Make it selected
+    item.addClass("selected");
+    item.parent().show();
+    
+    console.log("Find the child menu "+item.attr("child")+" and show it");
+    $("#"+item.attr("child")).show();
+    
+    position_menu(item);
   }
   
   $.fn.columnview = function(options){
-    var settings = $.extend({}, $.fn.columnview.defaults, options);
-    $("#outer li").each(function() {      
-      
-      var $this = $(this)
-      var $ul = $this.children('ul');
-      
-      $this.data('child', $ul);
-      
-      // hide it
-      $ul.hide();
-      
-      // Append it to the outer div
-      $ul.appendTo('#outer');
-      
-      // set it's parent attribute
-      $ul.attr("parent", $this.attr("id"));
-      
-    }).click(function() {
+    var settings = $.extend({}, $.fn.columnview.defaults, options);  
+    $("#outer li").each(function() {
       
       var $this = $(this);
+      var $ul = $(this).children('ul');
       
-      // Make it selected
-      $this.addClass('selected');
+      $(this).data('child', $ul);
       
+      $ul.hide();
+      $ul.appendTo('#outer');
       
-      // left
-      var this_left = $this.parent().offset().left;
-      var panel_left = $this.parent().parent().offset().left;
-      var panel_scroll_left = $this.parent().parent().scrollLeft();
-      var left = (this_left-panel_left)+panel_scroll_left
-      var width = $this.parent().width();
+      $ul.attr("id", "child_of_"+$(this).attr("id"));
+      $ul.addClass("child_menu");      
+
+      $(this).attr("child", $ul.attr("id"));
+
       
-      $this.data('child').css({
-        left: left + width,
-        top: 0
-      });
-      
-      $this.data('child').show();
-      $this.siblings('.selected').each(deselect);
-      
+    }).click(function() {
+      select_item($(this));
     });
     
     // attach sortable out here so that it hits all the ULs
     var sortOpts = {
+      
       connectWith: '#outer ul',
       containment: '#outer',
       appendTo: '#outer',
       helper: 'clone',
       stop: function(event, ui){
-        var id = $(ui.item[0]).attr("id");
-        var $this = $(this);
-        $this.siblings('.selected').each(deselect);
       },
       receive: function(event, ui) { 
-        var id = $(ui.item[0]).attr("id");
-        var child_id = $(event.target).attr("parent");        
-        if (id == child_id) { 
+        var this_id = $(ui.item[0]).attr("id");
+        var child_id = $(event.target).attr("parent");
+        if (this_id == child_id) { 
           $(ui.sender).sortable('cancel'); 
         }
-      }
+      } 
     };
     
     $("#outer ul").sortable(sortOpts);
