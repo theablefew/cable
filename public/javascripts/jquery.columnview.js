ee314cc5cb270dabe27b670db70e464c5b9ef098
Michael Bishop <mike@theablefew.com>
2011-03-02 21:21:56 -0600

    wip - closest working menu yet

--- public/javascripts/jquery.columnview.js
+++ public/javascripts/jquery.columnview.js
@@ -11,77 +11,90 @@
  * Dual licensed under MIT and GPL.
  */
 
-(function($){
+(function($){    
   
-  function hide_all_child_menus(){
-    $(".child_menu").hide();
-  }
-  
-  function show_child(item){
-    var child_menu = $("[parent="+item.attr("id")+"]");
-    child_menu.show();
-  }
-  
-  function show_parents(item){
-    var parent_menu = $(item).parent();
+  function unfold(){
 
-    parent_menu.show();
-    if(parent_menu.attr("parent")){
-      show_parents(parent_menu);
-    }
-    
-  }
-  
-  function select_item(item){
-    
-    $(".selected").removeClass("selected");
-    item.addClass("selected");
-    hide_all_child_menus();
-    show_child(item);
-    show_parents(item);
-    
-  }
-  
-  $.fn.columnview = function(options){
-    var settings = $.extend({}, $.fn.columnview.defaults, options);  
+    $("#outer ul").sortable('destroy').show();
+
     $("#outer li").each(function() {
-
-      var $this = $(this);
-      var parent_id = $(this).attr("id")
-      var $ul = $(this).children('ul');
-      
-      $(this).data('child', $ul);
-      
-      $ul.hide();
-      $ul.appendTo('#outer');
-      $ul.attr("parent", parent_id);
-      $ul.addClass("child_menu");
-      
-    }).click(function() {
-      select_item($(this));
+      var $ul = $(this).data('childUl');
+      if ($ul) $ul.appendTo(this);
     });
+
+    // jsut for display purpose lets get it out of the style area:
+    $("#outer > ul").appendTo("#unfolded");
+
+    function walkIds($ul) {
+      var obj = {}, children = $ul.children('li');
+      children.each(function() {
+        obj[this.id] = walkIds($(this).children('ul'));
+      });
+      return obj;
+    }
+    var idMap = walkIds($('#unfolded > ul'));
+    console.log(idMap);
+    $("<div>").text(JSON.stringify(idMap)).appendTo('body');
+  }
+
+  function select_item(item){
+    var $this = $(item);
+    $this.addClass('selected').data('childUl').css({
+      left: $this.parent().position().left + $this.parent().width(),
+      top: 0
+    }).show();
+    $this.siblings('.selected').each(deselect);
+    var the_text = ""
+    $("#outer .selected").each(function(){
+      the_text += $(this).text();
+      the_text += " &raquo; "
+    });
+    $("#thecurrent").html("Current Selection &raquo;"+the_text);
+  }
     
-    // attach sortable out here so that it hits all the ULs
-    var sortOpts = {
-      
+  function deselect() {
+    var $ul = $(this).removeClass('selected').data('childUl');
+    if ($ul) {
+      $ul.hide().find('li.selected').removeClass('selected').each(deselect);
+    }
+  }
+
+  $.fn.columnview = function(options){
+    var idCounter = 0;
+    $("#outer li").each(function() {
+      var $this = $(this), $ul = $this.children('ul');
+      $this.attr('id', 'li'+(idCounter++));
+      $this.data('childUl', $ul);
+      if($ul.children().size()>0){
+        $this.addClass("has-children");
+      };
+      $ul.attr("parent",$(this).attr("id"));
+      $ul.hide().appendTo('#outer');
+    }).click(function() {
+      select_item(this);
+    });
+
+    var sortable_options = {
       connectWith: '#outer ul',
+      containment: '#outer',
       appendTo: '#outer',
       helper: 'clone',
       stop: function(event, ui){
+        console.log(ui.item[0]);
+        select_item(ui.item[0]);
       },
       receive: function(event, ui) { 
-        
         var this_id = $(ui.item[0]).attr("id");
         var child_id = $(event.target).attr("parent");
-        
         if (this_id == child_id) { 
           $(ui.sender).sortable('cancel'); 
+          alert("Can't drag parent into itself.");          
         }
-      } 
+      }
     };
-    
-    $("#outer ul").sortable(sortOpts);
-    
-  };
-
+    $("#outer").after("<div id='unfold'>Unfold</div>");
+    $("#outer").after("<div id='unfolded'></div>");
+    $("#outer ul").sortable(sortable_options);
+    $("#unfold").click(unfold);
+  }
 })(jQuery);
