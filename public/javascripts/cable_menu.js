1ef4a73df3a0406db069ecbafd43cca1213d8aa4
Michael Bishop <mike@theablefew.com>
2011-03-06 13:55:26 -0600

    wip

--- public/javascripts/cable_menu.js
+++ public/javascripts/cable_menu.js
@@ -1,40 +1,117 @@
 (function($){    
   
   function save_tree(){
-    $(".cable_menu li").each(function() {
-      var $ul = $(this).data('childUl');
-      if ($ul) $ul.appendTo(this);
+    $(".cable_menu_wrapper li").each(function() {
+      console.log($(this).data()); 
+      console.log("----");
     });
     var mylist = $('.cable_menu_wrapper > ul').serialize_list();
-    $.post("/admin/menus/sort", mylist);
+    console.log(mylist);
+    // $.post("/admin/menus/sort", mylist);
   }
   
-  function select_item(item){
-    var $this = $(item);
-    $this.addClass('selected').data('childUl').css({
-      left: $this.parent().position().left + $this.parent().width(),
-      top: 0
-    }).show();
-    $this.siblings('.selected').each(deselect);
+  function show_add_menu(item){
+    $("#add-menu").remove();
+    var $add = $("<li id='add-menu'>Add Menu &#8230;</li>").appendTo($(item));
+    $add.click(function(){
+      $("#dialog").remove();
+
+      var id = $(item).attr("parent");      
+      $add_dialog = $(("<div id='dialog'></div>")).load("/admin/menus/new?parent_id="+id).appendTo("body");
+      
+      $add_dialog.dialog({
+        title: 'Add Menu Item',
+  			height: 300,
+  			width: 350,
+  			modal: true,
+  			buttons: {
+  			  Cancel: function() {
+  					$( this ).dialog( "close" );
+  				},
+  				Create: function() {
+  				}
+  			},
+  			close: function() {
+  			}
+  		});
+  		$add_dialog.dialog( "option", "title", 'Dialog Title' );
+    })
+  }
+  
+  function hide_add_menu(item){
+    $("#add-menu").remove();
+  }
+  
+  function update_breadcrumb(){
     var items = new Array();
-    $("..cable_menu_wrapper .selected").removeClass("last");
-    $("..cable_menu_wrapper .selected").each(function(){
+    $(".cable_menu_wrapper .selected").removeClass("last");
+    $(".cable_menu_wrapper .selected").each(function(){
       items.push($(this).text());
     });
     
-    $(".current_selection").html("<strong>Current Selection:</strong>"+items.join(" &raquo; "));
-    $(".cable_menu_wrapper .selected").last().addClass("last");
+    $("#selection p").html(items.join(" &raquo; "));
+    $(".cable_menu_wrapper .selected").last().addClass("last");    
   }
   
-  function deselect() {
-    var $ul = $(this).removeClass('selected').data('childUl');
-    if ($ul) {
-      $ul.hide().find('li.selected').removeClass('selected').each(deselect);
-    }
-  }
-  
-  $.fn.serialize_list = function(options) {
+  function select_item(item,wrapper){
+    $item = $(item);
+    id = $(item).attr("id");
 
+    $child = $("[parent="+id+"]")
+    
+    if($child){
+      $child.css({
+        left: $item.parent().position().left + $item.parent().width()+3+$(".cable_menu_wrapper").scrollLeft(),
+        top: 0
+      }).show();      
+    }
+    
+    $item.addClass("selected");
+    $item.siblings('.selected').each(function(){
+      deselect(this);
+    });
+    $child.children().each(function(){
+      deselect(this);
+    });
+    
+    $(wrapper).append($item.parent());
+    update_breadcrumb();
+  }
+  
+  function deselect(item) {
+    var $item = $(item);
+    $item.removeClass('selected');
+    id = $item.attr("id");
+
+    $child = $("[parent="+id+"]");
+
+    $child.hide().find("li").each(function(){
+      deselect(this);
+    });
+    
+  }
+  
+  function explode(item, wrap){
+    $wrap = $(wrap)
+    $li = $(item);
+    $ul = $li.children('ul');     
+    console.log($li);
+    if($ul.children().size() > 0){
+      $cul = $("<ul/>").attr("parent", $li.attr("id")).addClass("cable_menu").appendTo($wrap);        
+      $ul.children().each(function(){
+        var $cli = $(this).clone();
+        if($cli.children().size() > 0){
+          $cli.addClass("has-children");
+        }
+        $cli.appendTo($cul);
+        $cli.data("ChildUL",$ul);
+        $cli.find("ul").remove();
+      });
+      $cul.hide();
+    }
+    
+  }
+  $.fn.serialize_list = function(options) {
     var defaults = {
       attributes: ['id', 'class'],
       allow_nesting: true, 
@@ -89,46 +166,47 @@
   };
   
   $.fn.cable_menu = function(options){
+    var $tree = $(this);
+    var $wrap = $("<div class='cable_menu_wrapper'></div>").insertAfter($tree);
     
-    $(this).wrap("<div class='cable_menu_wrapper' />");
-    $(this).addClass("cable_menu");
-    $(".cable_menu li").each(function() {
-      var $this = $(this), $ul = $this.children('ul');
-      $this.data('childUl', $ul);
-      if($ul.children().size()>0){
-        $this.addClass("has-children");
-      };
-      $ul.attr("parent",$(this).attr("id"));
-      $ul.addClass("cable_menu");
-      $ul.hide().appendTo('.cable_menu_wrapper');
-    }).click(function() {
-      select_item(this);
+    $tree.find("li").each(function() {
+      explode(this,$wrap);
     });
-
+    // $tree.children("li").each(function(){
+    //   explode(this,$wrap);
+    // })
+    
+    
+    $(".cable_menu li").click(function(){
+      select_item(this, $wrap);
+    });
+    
+    $tree.hide();
+    
+    
     var sortable_options = {
+      items: "li:not(#add-menu)",
       connectWith: '.cable_menu_wrapper ul',
       appendTo: '.cable_menu_wrapper',
       helper: 'clone',
+      placeholder: "ui-state-highlight",
       stop: function(event, ui){
         select_item(ui.item[0]);
+      },
+      update: function(event, ui) {
+        
       },
       receive: function(event, ui) { 
         var this_id = $(ui.item[0]).attr("id");
         var child_id = $(event.target).attr("parent");
-        
         var size = $(".cable_menu .selected").size()-1;
         
-        console.log("Child ID:"+child_id);
-        console.log("This ID:"+this_id);
-        
         $(".cable_menu .selected").each(function(index){
-          console.log(this_id + " - " + $(this).attr("id"));
           if(size > index ){
             if($(this).attr("id") == this_id){
               $(ui.sender).sortable('cancel');
             }
           }else{
-            console.log("It's the last one. Do something slightly different.");
             if(child_id == this_id){
               $(ui.sender).sortable('cancel');
             }
@@ -136,11 +214,18 @@
         });
       }
     };
-    $(".cable_menu_wrapper").after("<div id='unfold'>Unfold</div>");
-    $(".cable_menu_wrapper").before("<div class='current_selection'><strong>Current Selection:</strong></div>")
-    $(".cable_menu_wrapper").after("<div id='unfolded'></div>");
-    $(".cable_menu_wrapper ul").sortable(sortable_options);
-    $("#unfold").click(save_tree);
+    
+    $wrap.find("ul").sortable(sortable_options);
+    $wrap.find("ul").mouseenter(function() {
+      show_add_menu(this);
+     }).mouseleave(function(){
+      hide_add_menu(this);       
+     });
+    $wrap.before("<div id='selection'><strong>Current Selection:</strong><p>&nbsp;</p></div>");
+    $wrap.after("<div id='save_tree'><a class='button'>Save Tree</a></div>");
+    $("#save_tree").click(function(){
+      save_tree();
+    });
   }
 
 })(jQuery);
