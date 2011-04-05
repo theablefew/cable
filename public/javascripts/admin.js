e59c5e4c7d633644e9b90f00e5c900f544c0110b
Michael Bishop <mike@theablefew.com>
2011-04-04 22:03:33 -0500

    cleanup

--- public/javascripts/admin.js
+++ public/javascripts/admin.js
@@ -1,34 +1,32 @@
 $(document).ready(function(){
   $('.sortable-table').tablesorter();
-  $().ready(function() {
-    $('textarea.rich_editor').tinymce({
-      script_url : '/javascripts/tinymce/tiny_mce.js',
-      theme : "advanced",
-      plugins : "pdw,pagebreak,style,layer,table,save,advhr,advimage,advlink,emotions,iespell,inlinepopups,insertdatetime,preview,media,searchreplace,print,contextmenu,paste,directionality,fullscreen,noneditable,visualchars,nonbreaking,xhtmlxtras,template,advlist",
-      theme_advanced_buttons1 : "pdw_toggle,bold,italic,underline,strikethrough,|,justifyleft,justifycenter,justifyright,justifyfull,formatselect,cut,copy,paste,pastetext,pasteword",
-      theme_advanced_buttons2 : "search,replace,|,bullist,numlist,|,outdent,indent,blockquote,|,undo,redo,|,link,unlink,anchor,image,cleanup,help,code,preview",
-      theme_advanced_buttons3 : "tablecontrols,|,hr,removeformat,visualaid,|,sub,sup,|,media|,fullscreen",
-      theme_advanced_toolbar_location : "top",
-      theme_advanced_toolbar_align : "left",
-      theme_advanced_statusbar_location : "bottom",
-      theme_advanced_resizing : true,
-      theme_advanced_resize_horizontal : false,
-      pdw_toggle_on : 1,
-      pdw_toggle_toolbars : "2,3"
-      content_css : "/stylesheets/admin/custom_rich_editor.css"
-    });
+  $('.rich_editor').tinymce({
+    script_url : '/javascripts/tinymce/tiny_mce.js',
+    theme : "advanced",
+    plugins : "pdw,pagebreak,style,layer,table,save,advhr,advimage,advlink,emotions,iespell,inlinepopups,insertdatetime,preview,media,searchreplace,print,contextmenu,paste,directionality,fullscreen,noneditable,visualchars,nonbreaking,xhtmlxtras,template,advlist",
+    theme_advanced_buttons1 : "pdw_toggle,bold,italic,underline,strikethrough,|,justifyleft,justifycenter,justifyright,justifyfull,formatselect,cut,copy,paste,pastetext,pasteword",
+    theme_advanced_buttons2 : "search,replace,|,bullist,numlist,|,outdent,indent,blockquote,|,undo,redo,|,link,unlink,anchor,image,cleanup,help,code,preview",
+    theme_advanced_buttons3 : "tablecontrols,|,hr,removeformat,visualaid,|,sub,sup,|,media|,fullscreen",
+    theme_advanced_toolbar_location : "top",
+    theme_advanced_toolbar_align : "left",
+    theme_advanced_statusbar_location : "bottom",
+    theme_advanced_resizing : true,
+    theme_advanced_resize_horizontal : false,
+    pdw_toggle_on : 1,
+    pdw_toggle_toolbars : "2,3",
+    content_css : "/stylesheets/admin/custom_rich_editor.css"
   });
+
   $('.disabled-button').button({disabled: true});
-  // $('.yes_no :checkbox').iphoneStyle({
-  //   checkedLabel: 'YES',
-  //   uncheckedLabel: 'NO'
-  // });
   
   $(".collapsible ol").hide();
   $(".collapsible legend").click(function(){
     $(this).nextAll("ol").slideToggle(500);
-    $(this).parent().toggleClass("open")
+    $(this).parent().toggleClass("open");
   });
+  
+  $(".tabs").tabs({fx: { height: 'toggle'}});
+  
   $(".collapsible legend").append(" <span class='expand'>Expand</span> <span class='close'>Close</span>");
   $(".collapsible legend").css({"cursor":"pointer"});
   $(".selected").last().addClass("last");
