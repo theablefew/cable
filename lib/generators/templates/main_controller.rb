96a5766438c20063d2785721b8a6d56de35bc95e
Spencer Markowski <spencer@theablefew.com>
2011-07-06 01:45:59 -0400

    Adds special actions and templates to location tab

--- lib/generators/templates/main_controller.rb
+++ lib/generators/templates/main_controller.rb
@@ -1,5 +1,18 @@
 class MainController < ApplicationController
   
+  ## Cable::Helpers::CableControllerHelpers defines find_by_url method. Overwrite to make customized changes.
+  # def find_by_url
+  #   @location = Location.find_by_marketable_url( params[:url] ) || Location.find_by_url( "/#{params[:url]}" )
+  #   unless @location.nil?
+  #     @resource = @location.resource
+  #     @page_title =  (@location.title.nil?) ? "" : @location.title
+  #     send(@location.special_action) unless @location.special_action.blank?
+  #     render :action => :show
+  #   else
+  #     redirect_to "/"
+  #   end
+  # end
+  
   include Cable::Helpers::CableControllerHelpers
   
   def index
