a4084f540ad280eafa13f4d82de4555b89aec88a
Iouri Kostine <iouri@theablefew.com>
2011-07-08 13:18:58 -0500

    Removes baked in tiny_mce, adds tiny_mce helper to admin layout, adds uses_tiny_mce to admin controller

--- app/controllers/admin_controller.rb
+++ app/controllers/admin_controller.rb
@@ -1,6 +1,6 @@
 class AdminController < Cable::CableAdminController
   layout proc{ |c| c.request.xhr? ? false : "admin" }  
-  
+  #uses_tiny_mce :options => {:editor_selector => "html_editor"}
   def index
   end
   
