0611b81d98b5dd5d8b55dadf80a964ad5045b983
Spencer Markowski <spencer@theablefew.com>
2011-09-01 19:43:46 -0400

    Fixes syntax errors in menu views, adds smart find to maskable finder by allowing visible or active selection, adds find_by_url_mask to helpers

--- lib/cable/url_mask.rb
+++ lib/cable/url_mask.rb
@@ -9,7 +9,9 @@ class Cable::UrlMask < ActiveRecord::Base
     request = "/*" if request == "/"
     order = parts.to_enum(:each_with_index).collect{|part,index| parts.dup.slice(0..index).join("/") + "/*" }.flatten[0..-2].push( request )
     logger.info "Find Masks by: #{order}".color :yellow
-    UrlMask.find_all_by_url_mask( order ).where(:active => true)
+    maskables = UrlMask.where(:url_mask => order).select do |d|
+      (d.maskable.respond_to?(:active) and d.maskable.active?) || (d.maskable.respond_to?(:visible) and d.maskable.visible?) || (!d.maskable.respond_to?(:visible) and !d.maskable.respond_to?(:active))
+     end
   end
   
   def resource
